446abd03459844292f491ad5ef8e64787d8d602f
Amy Cui <littlealien@Littles-MBP.fios-router.home>
2017-03-28 07:40:03 -0700

    CoordinatorLayout in DetailActivity
    1. Correct fab_press_translation_z
    2. Add CoordinatorLayout in DetailActivity

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ScrollAwareFabBehavior.java
@@ -0,0 +1,76 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.content.Context;
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+import com.example.xyzreader.R;
+
+import java.util.List;
+
+/* This class is from CheeseSquare app. Github link here:
+* https://github.com/ianhanniballake/cheesesquare/commit/aefa8b57e61266e4ad51bef36e669d69f7fd749c
+*/
+
+public class ScrollAwareFabBehavior extends AppBarLayout.ScrollingViewBehavior {
+    public ScrollAwareFabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
+    private boolean mIsAnimatingOut = false;
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return super.layoutDependsOn(parent, child, dependency) ||
+                dependency instanceof FloatingActionButton;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final View child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout, final View child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0) {
+            // User scrolled down -> hide the FAB
+            List<View> dependencies = coordinatorLayout.getDependencies(child);
+            for (View view : dependencies) {
+                if (view instanceof FloatingActionButton) {
+                    ((FloatingActionButton) view).hide();
+                }
+            }
+        } else if (dyConsumed < 0) {
+            // User scrolled up -> show the FAB
+            List<View> dependencies = coordinatorLayout.getDependencies(child);
+            for (View view : dependencies) {
+                if (view instanceof FloatingActionButton) {
+                    ((FloatingActionButton) view).show();
+                }
+            }
+        }
+    }
+}
